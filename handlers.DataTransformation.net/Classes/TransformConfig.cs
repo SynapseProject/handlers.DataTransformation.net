cd078351987e59b68ba1f5264c5050bb2e9d96ab
Steve <steve.shortt@gmail.com>
2018-01-15 23:25:47 -0600

    arranging project files

---
+++ handlers.DataTransformation.net/Classes/TransformConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using YamlDotNet.Serialization;
+
+using Zephyr.DataTransformation;
+
+
+public class TransformConfig
+{
+    public FormatType InputType { get; set; } = FormatType.Json;
+    public FormatType OutputType { get; set; } = FormatType.None;
+    [YamlIgnore]
+    internal bool HasConvert { get { return OutputType != FormatType.None && InputType != OutputType; } }
+    [YamlIgnore]
+    internal bool InputTypeIsJson { get { return InputType == FormatType.Json; } }
+    [YamlIgnore]
+    internal bool InputTypeIsXml { get { return InputType == FormatType.Xml; } }
+
+    public override string ToString()
+    {
+        return $"InputType: {InputType}, OutputType: {OutputType}, HasConvert: {HasConvert}";
+    }
+}
--- handlers.DataTransformation.net/Classes/TransformHandler.cs
+++ handlers.DataTransformation.net/Classes/TransformHandler.cs
@@ -197,22 +197,6 @@ public class TransformHandler : HandlerRuntimeBase
     }
 }
 
-public class TransformConfig
-{
-    public FormatType InputType { get; set; } = FormatType.Json;
-    public FormatType OutputType { get; set; } = FormatType.None;
-    [YamlIgnore]
-    internal bool HasConvert { get { return OutputType != FormatType.None && InputType != OutputType; } }
-    [YamlIgnore]
-    internal bool InputTypeIsJson { get { return InputType == FormatType.Json; } }
-    [YamlIgnore]
-    internal bool InputTypeIsXml { get { return InputType == FormatType.Xml; } }
-
-    public override string ToString()
-    {
-        return $"InputType: {InputType}, OutputType: {OutputType}, HasConvert: {HasConvert}";
-    }
-}
 
 public class TransformParameters
 {
